f93ed3cbb1080c201dbfa8e6787adfe676be32b5
Blik-5 <svet26102001@gmail.com>
2025-03-14 12:40:11 +0300

    Svfunction

--- SvGitLabApp/Program.cs
+++ SvGitLabApp/Program.cs
@@ -10,6 +10,13 @@ namespace SvGitLabApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Функция F(x) = 5 * Cos(2*x)");
+            Console.Write("Введите x = ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double F; 
+            F = 5 * Math.Cos(2 * x); // Функция F(x)
+            Console.WriteLine("Функция F({0:0.##}) = {1:0.####}", x, F);
+            Console.ReadLine();
         }
     }
 }
